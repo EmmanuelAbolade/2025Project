19c84fce6383e2a566952977a91061cbf2e3db83
Emmanuel <144928280+EmmanuelAbolade@users.noreply.github.com>
2025-03-08 02:00:43 +0000

    Update Project

---
+++ README.md
@@ -0,0 +1,5 @@
+# 2025Project
+
+A website and application that transforms the hospitality experience for hotel guests; a full-featured digital concierge to provide a user-friendly platform for hotel guests to enhance their convenience, improve communication with hotel staff and deliver personalized services seamlessly.
+
+Link to Project Document: https://docs.google.com/document/d/1vwa7Akim4TS-9_qt-DupDD9QYkhZ0fvLyVeKOtOb3Vw/edit?usp=sharing
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./css/style.css";
+import "./css/bootstrap.min.css";
+import "./css/animate.css";
+import "./css/animate.min.css";
+import "./App.css";
+import Header from "./components/common/Header";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Home,
+  Booking,
+  Services,
+  AboutUs,
+  Contact,
+  PageNotFound,
+  Room,
+  Team,
+  Testimonial,
+} from "./pages/index";
+import Footer from "./components/common/Footer";
+export default function App() {
+  return (
+    <>
+      <div>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/rooms" element={<Room />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/testimonial" element={<Testimonial />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+    </>
+  );
+}
