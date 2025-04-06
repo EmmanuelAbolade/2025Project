9b10d7e6981dca82cd07eb3779c12bb2bab25e5f
Emmanuel <144928280+EmmanuelAbolade@users.noreply.github.com>
2025-04-06 01:20:43 +0100

    Updates

--- src/components/common/FormButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FormButton({ text, onClick }) {
-  return (
-    <button className="btn btn-primary w-100 mt-3" onClick={onClick}>
-      {text}
-    </button>
-  );
-}
